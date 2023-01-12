51732df6ad4d2f73f73bc7a34ce8b6f8cbe8dfa9
Rishabh <rish.09.rp@gmail.com>
2023-01-13 00:47:11 +0530

    12 jan done

--- Assets/Scripts/PlayerMovements.cs
+++ Assets/Scripts/PlayerMovements.cs
@@ -6,11 +6,14 @@ public class PlayerMovements : MonoBehaviour
 {
     private Rigidbody2D playerRb;
     private float dirX;
+    private Animator anim;
+    private SpriteRenderer spR;
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GameObject.Find("Player").GetComponent<Rigidbody2D>();
-
+        anim = GameObject.Find("Player").GetComponent<Animator>();
+        spR = GameObject.Find("Player").GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -20,7 +23,22 @@ public class PlayerMovements : MonoBehaviour
         playerRb.velocity = new Vector2(dirX * 7.0f, playerRb.velocity.y);
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            playerRb.velocity = new Vector2(playerRb.velocity.x, 3);
+            playerRb.velocity = new Vector2(playerRb.velocity.x, 10f);
+        }
+        if (dirX > 0f)
+        {
+            anim.SetBool("Running", true);
+            spR.flipX = false;
+
+        }
+        else if(dirX <0f)
+        {
+            anim.SetBool("Running", true);
+            spR.flipX = true;
+        }
+        else
+        {
+            anim.SetBool("Running", false);
         }
     }
 }
